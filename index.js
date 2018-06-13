ae684f0634838d9ff549c5f29ff9335247af71dc
caiyou <rodickcai@gmail.com>
2018-06-13 20:24:48 +0800

    UPD

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
-const compose = require('koa-compose')
+const fs = require('fs.promised')
 
-const logger = (ctx, next) => {
-  console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`)
-  next()
-}
-
-const main = function(ctx) {
-  const n = Number(ctx.cookies.get('view') || 0) + 1;
-  ctx.cookies.set('view', n);
-  ctx.response.body = n + ' views';
-}
-
-const middlewares = compose([logger, main])
-app.use(middlewares)
+app.use(async ctx => {
+  ctx.body = '<p>123</p>'
+})
 
 app.listen(3000)
 
