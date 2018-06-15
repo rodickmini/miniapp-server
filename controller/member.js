1d11f70148622c110f2a8b021419ec22c2ea83cd
caiyou <rodickcai@gmail.com>
2018-06-15 18:24:29 +0800

    UPDATE geohash 计算6位

--- controller/member.js
+++ controller/member.js
@@ -4,7 +4,7 @@ const debug = require('debug')('member')
 const getMembers = async (ctx, next) => {
   debug(ctx.request.body)
   const [lat, lon, userinfo] = [ctx.request.body.lat, ctx.request.body.lon, ctx.request.body.userinfo]
-  const hash = geohash.encode(lat, lon)
+  const hash = geohash.encode(lat, lon, 6)
   await ctx.redis.sadd(`pos:geohash:${hash}`, JSON.stringify(userinfo))
   const memberListArr = await ctx.redis.smembers(`pos:geohash:${hash}`)
   const memberList = memberListArr.map(m => {return JSON.parse(m)})
